c113f832a1bc7566c597976c6652e72d1affbe99
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-21 15:04:55 +0300

    Add the changes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-
+# from  flask_migrate import Migrate, MigrateCommand
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+
 app.config['SECRET_KEY'] = '5001bcb5b4c8439cee67d9964c03c6d2'
-app.config['SQLACHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes 
